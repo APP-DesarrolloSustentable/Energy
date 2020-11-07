a58b5845233b31dcee3cc0bf7b00a47b84076633
Christopher Valdez <30541517+ChrissValdez@users.noreply.github.com>
2020-11-07 15:19:01 -0600

    update front

--- index.py
+++ index.py
@@ -1,31 +1,60 @@
 from flask import Flask, render_template, redirect, url_for, request, session 
-import database, usuario, poshoUI, electrodomestico, grupo, consumo, tip, const
+import database, usuario, poshoUI, electrodomestico, grupo, consumo, tip, const, pymysql
 
 app = Flask(__name__)
 app.secret_key = "secret"
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    if not session["correo"] or not session["contraseña"]:
-        redirect(url_for("/Inisiar_Sesion"))
+    if session.get('logged_in'):
+        if session["logged_in"] == False:
+            IniciarSesion();  
+        else:
+            return redirect(url_for("inicio"))
     else:
-        if usuario.validar(session["correo"], session["contraseña"]):
-            redirect(url_for("/inicio"))
+        return redirect(url_for("IniciarSesion"))
 
 
-@app.route('/Inisiar_Sesion', methods=['POST', 'GET'])
+
+
+@app.route('/Iniciar_Sesion', methods=['POST', 'GET'])
 def IniciarSesion():
     if request.method == "POST":
-            session["correo"] = request.form["correo"] 
-            session["contraseña"] = request.form["contraseña"]
-            return redirect(url_for("/"))
+        correo = request.form["correo"] 
+        contraseña = request.form["contraseña"]
+        if(usuario.validar(correo , contraseña)):
+            session["correo"] = correo 
+            session["contraseña"] = contraseña
+            session["logged_in"] = True
+            return redirect(url_for("inicio"))
+        else: 
+            return render_template('Iniciar_Sesion.html')
     else:
         return render_template('Iniciar_Sesion.html')
 
+
+@app.route('/Registrate', methods=['POST', 'GET'])
+def CrearCuenta():
+    if request.method == "POST":
+       return render_template('Registrate.html')
+    else:    
+        return render_template('Registrate.html')
+
+
 @app.route('/inicio', methods=['POST', 'GET'])
 def inicio():
-    return render_template('inicio.html')
+    if session.get('logged_in'):
+        if session["logged_in"] == True:
+            return render_template('inicio.html', sesion = session)  
+        else:
+            return redirect(url_for("index"))
+    else:
+        return redirect(url_for("index"))
 
+@app.route('/cerrar_sesion', methods=['POST', 'GET'])
+def cerrar_sesion():
+    session.clear()
+    return redirect(url_for("index")) 
 
 if __name__ == '__main__':
     app.run(debug=True)
