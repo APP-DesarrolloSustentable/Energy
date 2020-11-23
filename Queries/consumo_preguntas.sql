c7676ffd2d6e682e5c4684ebaf77cf648856c22b
Rodrigo Gómez Maitret <>
2020-11-22 18:56:24 -0600

    Queries Consumo

---
+++ Queries/consumo_preguntas.sql
@@ -0,0 +1,34 @@
+START TRANSACTION;
+
+DELIMITER $$
+DROP PROCEDURE IF EXISTS consumo_preguntas $$
+CREATE PROCEDURE consumo_preguntas()
+BEGIN
+
+	SET @g_id = 17;
+	
+	SELECT	DISTINCT electrodomestico.id_electrodomestico,
+			electrodomestico.nombre,
+			pregunta.pregunta,
+			pregunta.respuesta_1,
+			pregunta.respuesta_2,
+			pregunta.respuesta_3
+	FROM	grupo_electrodomestico,
+			electrodomestico,
+            electrodomestico_pregunta,
+			pregunta
+	WHERE
+			grupo_electrodomestico.id_grupo = @g_id
+			AND grupo_electrodomestico.id_electrodomestico = electrodomestico.id_electrodomestico
+			AND electrodomestico.id_electrodomestico = electrodomestico_pregunta.id_electrodomestico
+			AND electrodomestico_pregunta.id_pregunta = pregunta.id_pregunta;
+			
+
+			
+
+END $$
+DELIMITER ;
+
+CALL consumo_preguntas();
+
+COMMIT;
