a3fea0677251a0dcbcfd10ec854f8b2fc9887c33
Rodrigo <green.tie.rgm@gmail.com>
2020-11-29 22:17:50 -0600

    Listado de consumo en meses.

--- Queries/SQL/consumo_lista_mes.sql
+++ Queries/SQL/consumo_lista_mes.sql
@@ -5,11 +5,11 @@ Este query te trae el consumo total del mes dado un id_grupo y una fecha.
 START TRANSACTION;
 
 DELIMITER $$
-DROP PROCEDURE IF EXISTS consumo_total_mes $$
-CREATE PROCEDURE consumo_total_mes(g_id INT, mes DATE)
+DROP PROCEDURE IF EXISTS consumo_lista_mes $$
+CREATE PROCEDURE consumo_lista_mes(g_id INT, mes DATE)
 BEGIN
 
-    SELECT  SUM(consumo_electrico) AS Consumo_Mes
+    SELECT  fecha, consumo_electrico
     FROM    consumo
     WHERE   MONTH(fecha)=MONTH(mes)
             AND YEAR(fecha)=YEAR(mes);
