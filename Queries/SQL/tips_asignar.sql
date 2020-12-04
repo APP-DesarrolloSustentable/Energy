c0924af06b5bd683c67987076b4ae61dd0b5a79d
Rodrigo <green.tie.rgm@gmail.com>
2020-12-04 13:53:19 -0600

    Tips de 3 a 5.

--- Queries/SQL/tips_asignar.sql
+++ Queries/SQL/tips_asignar.sql
@@ -1,5 +1,5 @@
 /*
-Este query asigna (3) tips aleatorios a un usuario si ha pasado más de una
+Este query asigna (5) tips aleatorios a un usuario si ha pasado más de una
 semana desde la última vez que se asignaron tips.
 Regresa 1 si se asignaron, 0 si no.
 */
@@ -17,20 +17,16 @@ BEGIN
         WHERE   id_usuario=u_id
     );
 
-    IF (@diff >= 7) THEN
+    IF ((@diff>=7) OR (@diff IS NULL)) THEN
         CREATE TEMPORARY TABLE temp_tips
             SELECT      DISTINCT idtip
             FROM        tip, usuario
             WHERE       usuario.id_usuario=u_id
                         AND usuario.arquetipo=tip.arquetipo
             ORDER BY    RAND()
-            LIMIT       3;
+            LIMIT       5;
 
-        IF (NULL=(
-            SELECT  id_usuario
-            FROM    tip_usuario
-            WHERE   id_usuario=u_id
-        )) THEN
+        IF (@diff IS NULL) THEN
             INSERT INTO tip_usuario(id_usuario)
             VALUES (u_id);
         END IF;
@@ -40,12 +36,27 @@ BEGIN
                 id_tip_1=(
                     SELECT  idtip
                     FROM    temp_tips
-                    LIMIT   1,1
+                    LIMIT   0,1
                 ),
                 id_tip_2=(
                     SELECT  idtip
                     FROM    temp_tips
+                    LIMIT   1,1
+                ),
+                id_tip_3=(
+                    SELECT  idtip
+                    FROM    temp_tips
                     LIMIT   2,1
+                ),
+                id_tip_4=(
+                    SELECT  idtip
+                    FROM    temp_tips
+                    LIMIT   3,1
+                ),
+                id_tip_5=(
+                    SELECT  idtip
+                    FROM    temp_tips
+                    LIMIT   4,1
                 )
         WHERE   id_usuario=u_id;
 
